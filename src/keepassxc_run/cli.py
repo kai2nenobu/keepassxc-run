5f4aeef93b0c41f9f82118d67934b1b7a73be917
Tsunenobu Kai <kai2nenobu@gmail.com>
2024-10-04 22:58:42 +0900

    feat(feat): invoke command specified as CLI arguments

--- src/keepassxc_run/cli.py
+++ src/keepassxc_run/cli.py
@@ -1,5 +1,15 @@
+import argparse
+import sys
+import subprocess
+
+
 def main():
-    print("Hello from keepassxc-run!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", nargs="+", help="command to execute")
+    args = parser.parse_args(sys.argv[1:])
+
+    process = subprocess.run(args=args.command, check=False)
+    sys.exit(process.returncode)
 
 
 if __name__ == "__main__":
