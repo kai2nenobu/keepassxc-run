91105f3b16e1a19061f2ffcd05dae64de42b0d74
Tsunenobu Kai <kai2nenobu@gmail.com>
2024-11-21 08:34:52 +0900

    feat(cli): integrate git-credential-keepassxc fetching from KeepassXC
    Added a function to retrieve credentials from KeepassXC via the `git-credential-keepassxc` command. This allows for dynamic replacement of environment variables that start with "keepassxc://" in the subprocess execution context. This enhancement improves security by facilitating seamless access to stored credentials while executing commands through the CLI without exposing sensitive information.

--- src/keepassxc_run/cli.py
+++ src/keepassxc_run/cli.py
@@ -1,6 +1,23 @@
 import argparse
+import json
+import os
 import sys
 import subprocess
+
+
+def _git_credential_keepassxc(url: str) -> str:
+    """Fetch a credential value by 'git-credential-keepassxc'"""
+    exe = "git-credential-keepassxc"
+    stdin = f"url={url}"
+    output = subprocess.check_output(
+        [exe, "--unlock", "10,3000", "get", "--json", "--advanced-fields"], input=stdin.encode("utf-8")
+    )
+    credential = json.loads(output)
+    attribute = url.split("/")[-1]
+    if attribute in ("username", "password", "url"):
+        return credential[attribute]
+    else:
+        return credential["string_fields"][attribute]
 
 
 def main():
@@ -8,7 +25,11 @@ def main():
     parser.add_argument("command", nargs="+", help="command to execute")
     args = parser.parse_args(sys.argv[1:])
 
-    process = subprocess.run(args=args.command, check=False)
+    envs = os.environ.copy()
+    for key, value in envs.items():
+        if value.startswith("keepassxc://"):
+            envs[key] = _git_credential_keepassxc(value)
+    process = subprocess.run(args=args.command, check=False, env=envs)
     sys.exit(process.returncode)
 
 
