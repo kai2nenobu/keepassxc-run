f7d595ffd80f820a65919174cca589e7e3048ca4
Tsunenobu Kai <kai2nenobu@gmail.com>
2024-11-28 11:48:26 +0900

    refactor(cli): extract environment variable reading into separate function

--- src/keepassxc_run/cli.py
+++ src/keepassxc_run/cli.py
@@ -22,19 +22,26 @@ def _git_credential_keepassxc(url: str) -> str:
         return credential["string_fields"][attribute]
 
 
+def _read_envs(env_files: list[str]) -> dict[str, str]:
+    """Read environment variables from running environment and env files."""
+    envs = os.environ.copy()
+    for env_file in env_files:
+        env_file_values = dotenv_values(env_file)
+        envs.update(env_file_values)
+    # Fetch secret values from KeePassXC database
+    for key, value in envs.items():
+        if value.startswith("keepassxc://"):
+            envs[key] = _git_credential_keepassxc(value)
+    return envs
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("command", nargs="+", help="command to execute")
     parser.add_argument("--env-file", action="append", default=[], help="Enable Dotenv integration with specific Dotenv files to parse. For example: --env-file=.env")
     args = parser.parse_args(sys.argv[1:])
 
-    envs = os.environ.copy()
-    for env_file in args.env_file:
-        env_file_values = dotenv_values(env_file)
-        envs.update(env_file_values)
-    for key, value in envs.items():
-        if value.startswith("keepassxc://"):
-            envs[key] = _git_credential_keepassxc(value)
+    envs = _read_envs(args.env_file)
     process = subprocess.run(args=args.command, check=False, env=envs)
     sys.exit(process.returncode)
 
