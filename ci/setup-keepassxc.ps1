0433e039c1db01c1b3f29e029bbd5ffeb61b308e
Tsunenobu Kai <kai2nenobu@gmail.com>
2024-12-10 11:08:42 +0900

    ci: fix

--- ci/setup-keepassxc.ps1
+++ ci/setup-keepassxc.ps1
@@ -14,5 +14,3 @@ curl.exe -sSL $zip_url -o $zip_path
 $keepassxc_cli = 'C:\Program Files\KeePassXC\keepassxc-cli.exe'
 &$keepassxc_cli import --set-key-file .\tests\data\test_db.keyx .\tests\data\test_db.xml .\tests\data\test_db.kdbx
 
-# Launch KeePassXC
-$keepassxc = 'C:\Program Files\KeePassXC\KeePassXC.exe'; &$keepassxc --config .\tests\data\keepassxc.ini --keyfile .\tests\data\test_db.keyx .\tests\data\test_db.kdbx
