fb3c91e9797c6ca7b03612698b17d27ee9b1c7eb
Tsunenobu Kai <kai2nenobu@gmail.com>
2025-02-01 22:31:36 +0900

    build: add a chocolatey package

---
+++ package/chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$url = 'https://github.com/kai2nenobu/keepassxc-run/releases/download/v0.1.0/keepassxc-run_Windows_x86_64.zip'
+$checksum = 'A53BE240857F51609242E489EE99561647C47C220300C99B70C707282554E0B9'
+$packageArgs = @{
+    PackageName = 'keepassxc-run'
+    Url = $url
+    Checksum = $checksum
+    ChecksumType = 'sha256'
+    UnzipLocation = $toolsPath
+}
+
+## Download and unpack a zip file - https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+Install-ChocolateyZipPackage @packageArgs
